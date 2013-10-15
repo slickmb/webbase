a7b3315010abf54b06ed4a39b048af30d7b638f3
Matthew R. Blasius <matthew.blasius@gmail.com>
2013-10-15 14:39:01 -0400

    Expand inc compiler

--- build/compile-inc.sh
+++ build/compile-inc.sh
@@ -1,11 +1,14 @@
 
 echo compiling server side includes
-for inc in `ls ./build/stage/public/inc`
+for inc in `find ./build/stage/public -type f -iregex '.*\(inc\|inc\.html\)'`
 do
-    echo $inc
-    find ./build/stage -type f -iregex '.*\(html\|inc\)' | xargs -i{} sed -i -e "/$inc/{
+    sedInc=${inc##./build/stage/public/}
+    echo $sedInc
+    sedInc=`echo $sedInc | sed -e 's/[]\/()$*.^|[]/\\\&/g'`
+    sedInc="virtual=\"\\/\\?${sedInc}"
+    find ./build/stage -type f -iregex '.*\(html\|inc\)' | xargs -i{} sed -i -e "/$sedInc/{
                 s/.*//
-                r ./build/stage/public/inc/$inc
+                r $inc
             }" {}
 done
 
@@ -14,9 +17,14 @@ for html in `find ./build/stage/public -type f -iregex '.*\(html\|css\)'`
 do
     depth=`echo $html | perl -ne '$count=0; while(/\//g){++$count}; print "$count";'`
     depth=$((depth-4))
-    replace=`for ((i=0;i<$depth;++i)); do echo -n ..\\\\/; done;`
+    if [ "$depth" = "0" ]; then
+        replace=".\\/"
+    else
+        replace=`for ((i=0;i<$depth;++i)); do echo -n ..\\\\/; done;`
+    fi
+
     echo $html $depth $replace
-    sed -i -e "s/\"\//\"$replace/g" -e "s/'\//'$replace/g" $html
+    sed -i -e "s/\"\/\(\\w\|\"\|'\)/\"$replace\\1/g" -e "s/'\/\(\\w\|\"\|'\)/'$replace\\1/g" $html
 done
 
 rm -rf ./build/stage/public/inc
